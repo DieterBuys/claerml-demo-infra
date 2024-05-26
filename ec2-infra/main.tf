2d12cdc54f43bd0043f87fe3aef8dd2e0a8a0076
Dieter Buys <dieter.buys@gmail.com>
2024-05-26 00:23:16 -0700

    Added port mappings so the server can be reached

--- ec2-infra/main.tf
+++ ec2-infra/main.tf
@@ -75,6 +75,27 @@ resource "aws_security_group" "main_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8008
+    to_port     = 8008
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
