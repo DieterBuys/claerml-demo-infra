4d0cb0c64ebaf6834112874fb5c9e2b96f58a247
Dieter Buys <dieter.buys@gmail.com>
2024-06-01 21:31:34 -0700

    Fixed duplicate resources

--- infra/main.tf
+++ infra/main.tf
@@ -130,7 +130,6 @@ resource "aws_eip" "clearml_server_eip" {
   vpc = true
 }
 
-# Associate the Elastic IP with the EC2 instance
 resource "aws_eip_association" "clearml_server_eip_assoc" {
   instance_id   = aws_instance.clearml_server.id
   allocation_id = aws_eip.clearml_server_eip.id
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,3 @@
-# Allocate an Elastic IP
-resource "aws_eip" "clearml_server_eip" {
-  vpc = true
-}
-
-# Associate the Elastic IP with the EC2 instance
-resource "aws_eip_association" "clearml_server_eip_assoc" {
-  instance_id   = aws_instance.clearml_server.id
-  allocation_id = aws_eip.clearml_server_eip.id
-}
-
-# Output the Elastic IP address
 output "clearml_server_ip" {
   value = aws_eip.clearml_server_eip.public_ip
 }
